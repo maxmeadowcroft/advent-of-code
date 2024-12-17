645227618bb3adc2cb0ee907c8d5c831cd6d27a2
Maximus Meadowcroft <maxmeadowcroft61@gmail.com>
2024-12-16 22:48:02 -0700

    added part 2 for day one

--- day1.py
+++ day1.py
@@ -1,16 +1,31 @@
+"""
+PART 1
+"""
 
 with open("data/1.txt", "r") as file:
     lines = file.readlines()
 
-data_left = [int(line.split()[0]) for line in lines]
-data_right = [int(line.split()[1]) for line in lines]
+data_left: list[int] = [int(line.split()[0]) for line in lines]
+data_right: list[int] = [int(line.split()[1]) for line in lines]
 
 data_left.sort()
 data_right.sort()
 
-total_distance = 0
+total_distance: int = 0
 
 for i in range(len(data_left)):
     total_distance += abs(data_left[i] - data_right[i])
 
 print(total_distance)
+
+"""
+Part 2
+"""
+
+similarity = 0
+
+for i in range(len(data_left)):
+    similarity += data_left[i] * data_right.count(data_left[i])
+
+print(similarity)
+
